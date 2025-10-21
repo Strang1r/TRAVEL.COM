0d79ec2317ca646e1c51b150fbd9515a85cc8cbd
Alex Ma <YunMa@clarku.edu>
2025-10-21 17:52:25 -0400

    保存当前修改

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,17 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import { visualizer } from 'rollup-plugin-visualizer'
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    visualizer({
+      filename: "dist/stats.html",
+      open: false,
+      gzipSize: true,
+      brotliSize: true,
+    }),
+  ],
   server: {
     proxy: {
       '/api': {
@@ -10,6 +19,13 @@ export default defineConfig({
         changeOrigin: true,
         rewrite: path => path.replace(/^\/api/, ''),
       },
+    },
+  },
+  assetsInclude: ["**/*.woff", "**/*.woff2", "**/*.ttf"],
+  build: {
+    chunkSizeWarningLimit: 1000, // 根据需要调整阈值
+    rollupOptions: {
+      // 配置 manualChunks 做自定义分片
     },
   },
 })
